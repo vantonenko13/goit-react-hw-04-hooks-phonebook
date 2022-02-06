a1f169754d065f07ee7d1caf5eb125ad59a2bc8b
Vladymyr Antonenko <vladymyr.a13@gmail.com>
2022-02-06 23:02:34 +0200

    Updates

---
+++ precache-manifest.7c533bfe30fc32ad7a3c5cbaaf754922.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "029a7ba2ee747bf6f09d710128f8923f",
+    "url": "/goit-react-hw-04-hooks-phonebook/index.html"
+  },
+  {
+    "revision": "0022faf2d0c4f572aaf9",
+    "url": "/goit-react-hw-04-hooks-phonebook/static/css/main.427590ae.chunk.css"
+  },
+  {
+    "revision": "5cf3f553ba21ecf49faa",
+    "url": "/goit-react-hw-04-hooks-phonebook/static/js/2.005bd501.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/goit-react-hw-04-hooks-phonebook/static/js/2.005bd501.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0022faf2d0c4f572aaf9",
+    "url": "/goit-react-hw-04-hooks-phonebook/static/js/main.fe69eeb1.chunk.js"
+  },
+  {
+    "revision": "4dcff631daeb49597abe",
+    "url": "/goit-react-hw-04-hooks-phonebook/static/js/runtime-main.79e560b6.js"
+  }
+]);
